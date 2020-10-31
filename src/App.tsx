7229164c8a94ff99d457e40026887c95c9c4784c
Christopher <gjeltendigital@gmail.com>
2020-10-31 12:33:00 +0100

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+            <h1>yoo</h1>
+        </div>
+    );
 }
 
 export default App;
